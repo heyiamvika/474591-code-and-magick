e7a9edc2d1bf9986f7d67e0c0611b980526c8b47
Viktoriia Nosova <viktoriianosova@hotmail.com>
2018-04-20 15:14:04 +0800

    Add changes

--- js/setup.js
+++ js/setup.js
@@ -1,56 +1,47 @@
 'use strict';
 
-// 1;
-var setup = document.querySelector('.setup');
-setup.classList.remove('hidden');
-
-// 2
-
 var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+var wizards = [];
+
+var setup = document.querySelector('.setup');
+setup.classList.remove('hidden');
+var setupSimilar = document.querySelector('.setup-similar');
+setupSimilar.classList.remove('hidden');
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
+var generateRandomValue = function (array) {
+  return Math.floor(Math.random() * array.length);
+};
 
 var generateWizards = function (name, surname, coatColor, eyesColor) {
-
-  var wizards = [];
-
   for (var i = 0; i < 4; i++) {
-    var random = function (array) {
-      return Math.floor(Math.random() * array.length);
-    };
-
     var wizard = {
-      name: names[random(name)],
-      surname: surnames[random(surname)],
-      coatColor: coatColors[random(coatColor)],
-      eyesColor: eyesColors[random(eyesColor)]
+      name: names[generateRandomValue(name)],
+      surname: surnames[generateRandomValue(surname)],
+      coatColor: coatColors[generateRandomValue(coatColor)],
+      eyesColor: eyesColors[generateRandomValue(eyesColor)]
     };
-
     wizards.push(wizard);
   }
 
   return wizards;
 };
 
-var wizards = generateWizards(names, surnames, coatColors, eyesColors);
-
-// 3
-var similarListElement = document.querySelector('.setup-similar-list');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-
-var renderWizards = function (wizard) {
+var renderWizards = function (Wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
-  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name + ' ' + wizard.surname;
-  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  wizardElement.querySelector('.setup-similar-label').textContent = Wizard.name + ' ' + Wizard.surname;
+  wizardElement.querySelector('.wizard-coat').style.fill = Wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = Wizard.eyesColor;
 
   return wizardElement;
 };
 
-// 4
-var fillFragments = function () {
+var renderElements = function () {
   var fragment = document.createDocumentFragment();
 
   for (var i = 0; i < wizards.length; i++) {
@@ -60,27 +51,5 @@ var fillFragments = function () {
   similarListElement.appendChild(fragment);
 };
 
-fillFragments();
-
-// 5
-var setupSimilar = document.querySelector('.setup-similar');
-setupSimilar.classList.remove('hidden');
-
-// Another decision
-
-// var renderWizards = function () {
-//   var similarListElement = document.querySelector('.setup-similar-list');
-//   var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-//
-//   for (var i = 0; i < 4; i++) {
-//     var wizardElement = similarWizardTemplate.cloneNode(true);
-//
-//     wizardElement.querySelector('.setup-similar-label').textContent = wizards[i].name + ' ' + wizards[i].surname;
-//     wizardElement.querySelector('.wizard-coat').style.fill = wizards[i].coatColor;
-//     wizardElement.querySelector('.wizard-eyes').style.fill = wizards[i].eyesColor;
-//
-//     similarListElement.appendChild(wizardElement);
-//   }
-// };
-//
-// renderWizards();
+generateWizards(names, surnames, coatColors, eyesColors);
+renderElements();
